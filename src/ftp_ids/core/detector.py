3ec113c292c301de3cd3514fe5db098e62527f88
AmraniCh <elamrani.sv.laza@gmail.com>
2026-06-21 05:12:33 +0100

    refactor (detector): rename features_cols to feature_names and update model load/save logic

--- src/ftp_ids/core/detector.py
+++ src/ftp_ids/core/detector.py
@@ -13,7 +13,7 @@ class Detector:
         self.model_path = model_path
         self.contamination = contamination
         self.extractor = FeatureExtractor()
-        self.features_cols = FEATURE_NAMES
+        self.feature_names = FEATURE_NAMES
         self.scaler = StandardScaler()
         self.pipeline = None
         self.score_scaler = None
@@ -22,14 +22,18 @@ class Detector:
         if os.path.exists(self.model_path):
             saved_state = joblib.load(self.model_path)
             self.pipeline = saved_state.get('pipeline') 
-            self.score_scaler = saved_state.get('score_scaler') 
+            self.scaler = saved_state.get('scaler')
+            self.score_scaler = saved_state.get('score_scaler')
+            self.feature_names = saved_state.get('features') 
             return True
         return False
     
     def train(self, sessions):
         features = self.extractor.extract_batch(sessions)
         df = pd.DataFrame(features)
-        X = df[self.features_cols]        
+        X = df[self.feature_names] 
+
+        pprint(df)       
 
         # TODO consider to test with dynamic contamination based on session events length
         self.pipeline = Pipeline([
@@ -44,12 +48,16 @@ class Detector:
         self.pipeline.fit(X)
         
         raw_scores = self.pipeline.decision_function(X)
+        print("raw_scores", raw_scores)
         self.score_scaler = MinMaxScaler(feature_range=(0, 1))
         self.score_scaler.fit(raw_scores.reshape(-1, 1))
+        # print(self.score_scaler)
     
         joblib.dump({
             "pipeline": self.pipeline,
-            "score_scaler": self.score_scaler
+            "scaler": self.scaler,
+            "score_scaler": self.score_scaler,
+            "features": self.feature_names
         }, self.model_path)
 
         print(f"Model trained and saved to {self.model_path}")
@@ -59,4 +67,4 @@ class Detector:
     # def _vectorize(self, sessions):
     #     features = self.extractor.extract_batch(sessions)
         
-        
+
