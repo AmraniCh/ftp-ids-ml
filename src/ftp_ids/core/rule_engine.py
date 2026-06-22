83a621a267f209e3844cb222e82d077ddf03c39c
AmraniCh <elamrani.sv.laza@gmail.com>
2026-06-22 03:39:16 +0100

    feat (rule_engine): add FTP bounce detection rule

--- src/ftp_ids/core/rule_engine.py
+++ src/ftp_ids/core/rule_engine.py
@@ -10,10 +10,13 @@ class RuleEngine:
             'rule_id': 'VSFTPD_234_BACKDOOR',
             'name': "vsftpd 2.3.4 Backdoor (CVE-2011-2523)",
             'matched': self._backdoor()
+        }, {
+            'rule_id': 'FTP_BOUNCE_MGLNDD',
+            'name': "FTP bounce probe (MGLNDD scanner)",
+            'matched': self._ftp_bounce(),
         }]
 
     def _backdoor(self):
-        
         for event in self.session['events']:
             if (event['command'] == "USER"
                 and event['argument'] 
@@ -21,3 +24,11 @@ class RuleEngine:
                 return True
                 
         return False
+    
+    def _ftp_bounce(self):
+        for event in self.session['events']:
+            if (event['command'] and event['command'].find("MGLNDD_") != -1):
+                return True
+
+        return False
+
