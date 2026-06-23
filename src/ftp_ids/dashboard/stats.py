cffc47eb009377b7656f3832c05e6ee643b03370
AmraniCh <elamrani.sv.laza@gmail.com>
2026-06-23 05:36:47 +0100

    feat (stats): update compute_stats to include alerts and unique IPs

--- src/ftp_ids/dashboard/stats.py
+++ src/ftp_ids/dashboard/stats.py
@@ -3,13 +3,22 @@ from datetime import date
 import ftp_ids.config as config
 
 def compute_stats():
-    #storage = Storage()
-    
+    storage = Storage()
+    alerts = storage.load_alerts()
+    today = date.today().isoformat()
+    alerts_today = 0
+    unique_ips_today = set()
+
+    for _,row in alerts.iterrows():
+        if row['start_time'].startswith(today):
+            alerts_today += 1
+            unique_ips_today.add(row['src_ip'])
+
     return {
         "events_today": _count_events_today(),
-        # "alerts_today": alerts_today,
+        "alerts_today": alerts_today,
         # "pending": pending,
-        # "unique_ips_today": unique_ips_today,
+        "unique_ips_today": len(unique_ips_today),
     }
 
     
@@ -18,3 +27,6 @@ def _count_events_today():
     today_prefix = date.today().strftime("%a %b %e")
     with open(config.LOGS_PATH, "r", errors="replace") as f:
         return sum(1 for line in f if line.startswith(today_prefix))
+
+
+print(compute_stats())
